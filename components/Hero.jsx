a86d884b7bbc2c6d537d2ddbb4d2b0ef2f328c4c
dennyscode <sander.bewerbung@gmx.de>
2022-05-09 23:50:38 +0200

    delete loaders for img

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <div className="text-center bg-white pb-10">
       <div className="w-60 mx-auto">
-        <Image src={'/undraw_home_cinema_l7yl.png'} loader="custom" width={200} height={200} layout="responsive" />
+        <Image src={'/undraw_home_cinema_l7yl.png'} width={200} height={200} layout="responsive" />
       </div>
       <h1 className="text-2xl text-gray-700 uppercase font-bold">Welcome at WatchMe</h1>
       <p className="text-gray-500">Produce FILM feature, TELEVISION and GAME.</p>
--- components/MovieCard.jsx
+++ components/MovieCard.jsx
@@ -2,19 +2,13 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 function MovieCard({movie}) {
-
-  const myLoader = ({ src }) => {
-    return `https://image.tmdb.org/t/p/w500${src}`
-  }
-
   return (
     <Link href={`/movie/${movie.id}`}>
         <div className="bg-white shadow-sm rounded-md cursor-pointer">
             <Image 
-                // loader={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} 
+                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} 
                 width={700} 
-                height={800}
-                loader={myLoader(movie.poster_path)}
+                height={800} 
                 className="rounded-t-md"
             />
             <div className="px-6 py-2">
